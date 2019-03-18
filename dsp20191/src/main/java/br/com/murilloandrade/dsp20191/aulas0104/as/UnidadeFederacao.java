6d255b3244ef8a685dddeb65edd3bde76ffc79e8
murillodesu <murillodesu@gmail.com>
2019-03-17 23:22:43 -0300

    Removendo código inutilizado

--- dsp20191/src/main/java/br/com/murilloandrade/dsp20191/aulas0104/as/UnidadeFederacao.java
+++ dsp20191/src/main/java/br/com/murilloandrade/dsp20191/aulas0104/as/UnidadeFederacao.java
@@ -1,9 +1,5 @@
 package br.com.murilloandrade.dsp20191.aulas0104.as;
 
 public class UnidadeFederacao {
-    //public String idUF;
-    //public String siglaUF;
-    //public String nomeUF;
-    //public String codigoRegiaoUF;
     public String dadosUF;
 }
