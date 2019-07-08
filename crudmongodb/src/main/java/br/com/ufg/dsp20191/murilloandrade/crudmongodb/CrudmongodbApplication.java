31519e068e93ac0f087fea031117544fa51aeb10
murillodesu <murillodesu@gmail.com>
2019-07-08 10:46:49 -0300

    Atividade crud mongodb

---
+++ crudmongodb/src/main/java/br/com/ufg/dsp20191/murilloandrade/crudmongodb/CrudmongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufg.dsp20191.murilloandrade.crudmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmongodbApplication.class, args);
+	}
+
+}
---
+++ crudmongodb/src/main/java/br/com/ufg/dsp20191/murilloandrade/crudmongodb/controller/UserController.java
@@ -0,0 +1,81 @@
+package br.com.ufg.dsp20191.murilloandrade.crudmongodb.controller;
+
+import java.util.List;
+import br.com.ufg.dsp20191.murilloandrade.crudmongodb.repositorio.UserRepository;
+import br.com.ufg.dsp20191.murilloandrade.crudmongodb.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class UserController {
+
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        LOG.info("Getting all users.");
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    public User getUser(@PathVariable String userId) {
+        LOG.info("Getting user with ID: {}.", userId);
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public User addNewUsers(@RequestBody User user) {
+        LOG.info("Saving user.");
+        return userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
+    public Object getAllUserSettings(@PathVariable String userId) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            return user.getUserSettings();
+        } else {
+            return "User not found.";
+        }
+    }
+
+    @RequestMapping(value = "/settings/{userId}/{key}", method = RequestMethod.GET)
+    public String getUserSetting(@PathVariable String userId, @PathVariable String key) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            return user.getUserSettings().get(key);
+        } else {
+            return "User not found.";
+        }
+    }
+
+    @RequestMapping(value = "/settings/{userId}/{key}/{value}", method = RequestMethod.GET)
+    public String addUserSetting(@PathVariable String userId, @PathVariable String key, @PathVariable String value) {
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            user.getUserSettings().put(key, value);
+            userRepository.save(user);
+            return "Key added";
+        } else {
+            return "User not found.";
+        }
+    }
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable String userId){
+        LOG.info("Deleting user with ID: {}.", userId);
+        userRepository.delete(getUser(userId));
+    }
+}
